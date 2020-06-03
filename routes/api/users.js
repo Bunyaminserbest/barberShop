3658a4060a1ac881109f23de324f5321a9d32134
Serbest <bunyamin.srbst@gmail.com>
2020-06-03 13:36:15 +0200

    User registration OK

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 // @route   POST api/users
 // @desc    register user
@@ -18,13 +20,47 @@ router.post(
     ).isLength({ min: 6 }),
   ],
 
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('Users route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Create an instance of a user (doesn't save the user)
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Encrypt password
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
