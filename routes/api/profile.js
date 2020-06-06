e688b63fdbb7ed44a6acb57ee9006a61b6f2c49c
Serbest <bunyamin.srbst@gmail.com>
2020-06-06 13:16:18 +0200

    Create profile model & Get current User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @desc    test route
-// @access  public
+Profile = require('../../models/Profile');
+User = require('../../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Profile route');
+// @route   GET api/profile/me
+// @desc    Get current user profile
+// @access  Private
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (errors) {
+    console.error(errors.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
